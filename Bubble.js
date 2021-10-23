39442093f1b0de7dc7dadd91231f55d507496799
Jeet-rana <62884695+Jeet-rana@users.noreply.github.com>
2021-10-23 16:25:38 +0530

    Update Bubble.js

--- Bubble.js
+++ Bubble.js
@@ -1,17 +1,18 @@
-function Heap(){
-   for(let i=total_size/2-1;i>=0;i--) maxheapify(i,total_size);
-   for(let i=total_size-1,heap_size=total_size-1;i>0;i--){
-      swap(0,heap_size,"skyblue","green");
-      maxheapify(0,heap_size--);
+function Bubble(){
+   let lenth=total_size;
+   for(let i=0;i<lenth-1;i++){
+      for(let j=0;j<lenth-i-1;j++){
+         // changing To yellow color.
+         update_bar(bar_div[j],"yellow",bar_len[j]);
+         if(bar_len[j]>bar_len[j+1]){ 
+            // Changing to red color as they need swap.
+            swap(j,j+1,"red","red");
+         }
+         // need to change back its color.
+         update_bar(bar_div[j],"skyblue",bar_len[j]);
+      }
+      // changing color to Green as it is in correct position.
+      update_bar(bar_div[lenth-1-i],"green",bar_len[lenth-1-i]);
    }
    update_bar(bar_div[0],"green",bar_len[0]);
-}
-function maxheapify(id,size){
-   let left = 2*id+1,right = 2*id+2,high=id;
-   if(left<size && bar_len[left]>bar_len[high]) high=left;
-   if(right<size && bar_len[right]>bar_len[high]) high=right;
-   if(high!=id){
-      swap(id,high,"pink","pink");
-      maxheapify(high,size);
-   }
 }
